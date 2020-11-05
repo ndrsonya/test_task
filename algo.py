9db4f7e3fca857531bcf2144e45e8bb6606bf469
ndrsonya <ViktorAnderson@MBP-Viktor.home>
2020-11-05 14:54:13 +0200

    first commit

---
+++ algo.py
@@ -0,0 +1,48 @@
+# SPACE COMPLEXITY
+# Time complexity is O(n) - linear algorithm,
+# As we have one loop to go through all chars in string.
+
+# SPACE COMPLEXITY
+# Space complexity is O(n)
+
+filename = 'input.txt'
+inputList = []
+outputList = []
+
+# Read input from the file
+with open(filename) as f_obj:
+    for line in f_obj:
+        inputList.append(line.rstrip())
+
+
+# Function for converting strings into output list of Strings
+def convert_string(string):
+    length = len(string)
+    count = 1
+    result = []
+    i = 0
+    for i in range(0, length - 1):
+        if string[i] == string[i + 1]:
+            count += 1
+            i += 1
+        else:
+            result.append(string[i])
+            result.append(str(count))
+            count = 1
+            i += 1
+    result.append(string[length - 1])
+    result.append(str(count))
+    outputList.append("".join(str(x) for x in result))
+
+
+# Function call for converting inputList if the list is not empty
+if inputList:
+    for element in inputList:
+        convert_string(element)
+
+
+# Writing output to the text file
+filename2 = 'output.txt'
+with open(filename2, 'w') as f:
+    for element in outputList:
+        f.write(element + "\n")
